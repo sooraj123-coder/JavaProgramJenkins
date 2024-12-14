5085dc660d333042d94e1ada9b6c4f90e92e5796
sooraj <soorajmailme@gmail.com>
2024-12-14 23:56:40 +0530

    first commit

---
+++ my.java
@@ -0,0 +1,9 @@
+import java.sql.Date;
+
+public class my{
+    public static void main(String[] args) {
+        System.out.println("Hello Sooraj");
+        System.out.println("This is java program");
+        System.out.println(new Date(0));
+    }
+}
