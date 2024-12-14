0bc81d24d486e6957a41366545da882cf8f158b8
sooraj <soorajmailme@gmail.com>
2024-12-14 23:58:28 +0530

    Initial commit

--- my.java
+++ my.java
@@ -5,5 +5,6 @@ public class my{
         System.out.println("Hello Sooraj");
         System.out.println("This is java program");
         System.out.println(new Date(0));
+        System.out.println("Hello");
     }
 }
