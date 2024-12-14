0664ed4f040d407beb3b0da496ada4b1ecccefd7
sooraj <soorajmailme@gmail.com>
2024-12-15 00:07:14 +0530

    my new message

--- my.java
+++ my.java
@@ -2,9 +2,8 @@ import java.sql.Date;
 
 public class my{
     public static void main(String[] args) {
-        System.out.println("Hello Sooraj");
-        System.out.println("This is java program");
+        System.out.println("Hello Sooraj, This file has been modified");
         System.out.println(new Date(0));
-        System.out.println("Hello");
+        
     }
 }
